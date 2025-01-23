4b86ccdcc0705cf2bb024adac1c12b7374c1bb08
antonie <53366054+antonie-riziki@users.noreply.github.com>
2025-01-23 17:28:55 +0300

    Add files via upload
    uploaded the main file

---
+++ echofarm/app.py
@@ -0,0 +1,35 @@
+import streamlit as st 
+import pandas as pd 
+import numpy as np 
+import seaborn as sb 
+import matplotlib.pyplot as plt 
+import plotly.express as px
+import os
+import csv
+import sys
+
+
+
+home_page = st.Page("./pages/main.py", title="Research", icon=":material/analytics:")
+agri_shield_page = st.Page("./pages/agri_shield.py", title="agri shield", icon=":material/grass:")
+irrigate_page = st.Page("./pages/irrigAIte.py", title="irrigAIte", icon=":material/agriculture:")
+soilwise_page = st.Page("./pages/soilwise.py", title="Soilwise", icon=":material/yard:")
+
+
+
+pg = st.navigation([home_page, soilwise_page, irrigate_page, agri_shield_page])
+
+
+st.set_page_config(
+    page_title="EchoFarm",
+    page_icon="🌱🪴",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.echominds.africa',
+        'Report a bug': "https://www.echominds.africa",
+        'About': "# We are a leading insights and predicting big data application, Try *EchoFarm* and experience reality!"
+    }
+)
+
+pg.run()
